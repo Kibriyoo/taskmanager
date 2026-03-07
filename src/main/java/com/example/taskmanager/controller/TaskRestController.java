83b03d41c437daef953a522a5b1d07cdf963d54e
kibriyoo <mirashomirzeva@gmail.com>
2026-03-08 01:14:36 +0700

    commit

---
+++ src/main/java/com/example/taskmanager/controller/TaskRestController.java
@@ -0,0 +1,24 @@
+package com.example.taskmanager.controller;
+
+import com.example.taskmanager.model.Task;
+import com.example.taskmanager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskRestController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping("/save")
+    public Task saveTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @GetMapping("/all")
+    public Iterable<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
