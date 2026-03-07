31b8313b13c0e84b6b5a1f16073eebbfd2c14dc9
kibriyoo <mirashomirzeva@gmail.com>
2026-03-08 02:05:24 +0700

    commit

--- src/main/java/com/example/taskmanager/controller/TaskRestController.java
+++ src/main/java/com/example/taskmanager/controller/TaskRestController.java
@@ -2,7 +2,8 @@ package com.example.taskmanager.controller;
 
 import com.example.taskmanager.model.Task;
 import com.example.taskmanager.repository.TaskRepository;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tasks")
